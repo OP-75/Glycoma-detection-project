5a6113e193f8ff1a647c87eb7d6cd95c2e3cb85e
OP-75 <hparyani75@gmail.com>
2023-07-15 06:06:28 -0700

    made dataloader and initial model of CNN

---
+++ plottingMasks.py
@@ -0,0 +1,8 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import torch
+
+
+img = mpimg.imread(r"D:\Datasets\Glycoma dataset 1 - 6gb\ORIGA\Masks\645.png")
+plt.imshow(img)
+
